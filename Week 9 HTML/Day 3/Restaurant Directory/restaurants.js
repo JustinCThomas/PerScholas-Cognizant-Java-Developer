ad98e33e9faf2c5c2f82a9404ba93bb07782118b
Justin Thomas <ThomasCJustin@gmail.com>
2018-05-18 15:55:11 -0400

    Add files via upload

--- Week 9 HTML/Day 3/Restaurant Directory/restaurants.js
+++ Week 9 HTML/Day 3/Restaurant Directory/restaurants.js
@@ -4,28 +4,43 @@ const rightArrows = document.getElementsByClassName("right-arrow");
 const restaurantArrows = [leftArrows[0], rightArrows[0]];
 const foodArrows = [leftArrows[1], rightArrows[1]];
 const drinksArrows = [leftArrows[2], rightArrows[2]];
-const comboArrows = [leftArrows[3], rightArrows[3]];
+// const comboArrows = [leftArrows[3], rightArrows[3]];
 
 let restaurants = document.getElementsByClassName("restaurant");
 let foodItems = document.getElementsByClassName("food-img-container");
 let beverages = document.getElementsByClassName("beverages-img-container");
 
-addEvents(restaurantArrows, restaurants, "restaurantCount");
-addEvents(foodArrows, foodItems, "foodCount");
-addEvents(drinksArrows, beverages, "beverageCount");
 
-setFourOptions(restaurants);
-setFourOptions(foodItems);
-setFourOptions(beverages);
+let mcDonalds = [[], []];
+let burgerKing = [[], []];
 
-
-function setFourOptions (optionType) {
-	for (let i = 0; i < optionType.length; i++){
-		if (i > 3){
-			optionType[i].style.display = "none";
-		}
+for (let i = 0; i < foodItems.length; i++){
+	if (foodItems[i].classList.contains("mcdonalds")){
+		mcDonalds[0] = foodItems[i];	
+	} else if (foodItems[i].classList.contains("burger-king")){
+		burgerKing[0] = foodItems[i];	
 	}
 }
+
+for (let i = 0; i < beverages.length; i++){
+	if (beverages[i].classList.contains("mcdonalds")){
+		mcDonalds[1] = beverages[i];	
+	} else if (beverages[i].classList.contains("burger-king")){
+		burgerKing[1] = beverages[i];	
+	}
+}
+
+
+addEvents(restaurantArrows, restaurants, "restaurantCount");
+
+
+addEvents(foodArrows, mcDonalds[0], "foodCount");
+addEvents(drinksArrows, mcDonalds[1], "beverageCount");
+
+addEvents(foodArrows, burgerKing[0], "foodCount");
+addEvents(drinksArrows, burgerKing[1], "beverageCount");
+
+selectRestaurant();
 
 let countHolder = {
 	foodCount: 0,
@@ -33,26 +48,101 @@ let countHolder = {
 	restaurantCount: 0
 };
 
+restaurants[0].click();
+
+
+function selectRestaurant() {
+	for (let i = 0; i < restaurants.length; i++){
+		restaurants[i].addEventListener("click", function() {
+			if (!this.classList.contains("selected")){
+				for (let i = 0; i < restaurants.length; i++) {
+					restaurants[i].classList.remove("selected");	
+				}
+				this.classList.add("selected");
+				showRestaurantOptions(this);
+			}
+		});
+	}
+}
+
+function showRestaurantOptions(clickedElement) {
+	// show McDonalds
+	if (clickedElement.classList.contains("mcdonalds")){
+		let order = 1;
+		for (let i = 0; i < foodItems.length; i++){
+			if (foodItems[i].classList.contains("mcdonalds")){
+				foodItems[i].style.display = "inline-block";
+				foodItems[i].style.order = order;
+				order++;
+			} else {
+				foodItems[i].style.display = "none";
+				foodItems[i].style.order = 100;
+			}
+		}
+
+		order = 1;
+
+		for (let i = 0; i < beverages.length; i++){
+			if (beverages[i].classList.contains("mcdonalds")){
+				beverages[i].style.display = "inline-block";
+				beverages[i].style.order = order;
+				order++;
+			} else {
+				beverages[i].style.display = "none";
+				beverages[i].style.order = 100;
+			}
+		}
+		// show Burger King
+	} else if (clickedElement.classList.contains("burger-king")){
+		let order = 1;
+		for (let i = 0; i < foodItems.length; i++){
+			if (foodItems[i].classList.contains("burger-king")){
+				foodItems[i].style.display = "inline-block";
+				foodItems[i].style.order = order;
+			} else {
+				foodItems[i].style.display = "none";
+				foodItems[i].style.order = 100;
+			}
+		}
+		order = 1;
+		
+		
+		for (let i = 0; i < beverages.length; i++){
+			if (beverages[i].classList.contains("burger-king")){
+				beverages[i].style.display = "inline-block";
+				beverages[i].style.order = order;
+				order++;
+			} else {
+				beverages[i].style.display = "none";
+				beverages[i].style.order = 100;
+			}
+		}
+	}
+	countHolder.foodCount = 0;
+	countHolder.beverageCount = 0;
+	countHolder.restaurantCount = 0;
+}
+
 function addEvents(arrowSet, itemCategorySet, countName){
 	if (itemCategorySet.length >= 4) {
 		arrowSet[0].addEventListener("click", () => {
 			console.log("clicked");
 			countHolder[countName] -= 1;
 			countHolder[countName] = checkCount(countHolder[countName], itemCategorySet);
-			displayFoodItems(countHolder[countName], itemCategorySet);
+			displayItems(countHolder[countName], itemCategorySet);
 		});
 
 		arrowSet[1].addEventListener("click", () => {
 			console.log("clicked right button");
 			countHolder[countName] += 1;
 			countHolder[countName] = checkCount(countHolder[countName], itemCategorySet);
-			displayFoodItems(countHolder[countName], itemCategorySet);
+			displayItems(countHolder[countName], itemCategorySet);
 		});
 	}
 }
 
 
-function displayFoodItems(count, itemCategorySet){
+function displayItems(count, itemCategorySet){
 	for (let i = 0; i < itemCategorySet.length; i++){
 		itemCategorySet[i].style.display = "none";
 		itemCategorySet[i].style.order = 100;
@@ -61,7 +151,7 @@ function displayFoodItems(count, itemCategorySet){
 	let order = 1;
 
 	if (count >= 0) {
-		for (let i = count; i <= count + 3	; i++){
+		for (let i = count; i <= count + 3; i++){
 			if (i <= itemCategorySet.length - 1) {
 				itemCategorySet[i].style.display = "inline-block";
 				itemCategorySet[i].style.order = order;	
